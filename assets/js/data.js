5ccf094b98db763dbabe7f7f377b87fc4cc94a05
Virus 24 <kazouya44@gmail.com>
2024-04-12 18:15:06 +0200

    update protofolio

---
+++ assets/js/data.js
@@ -0,0 +1,168 @@
+const siteData = {
+
+  Projects: [
+    {
+      name: "Adham Fathallah Real Estate",
+      img: "/assets/images/projects/adhamfathallah.png",
+      link: "https://adhamfathallah.com/",
+      category: "web development",
+    },
+    {
+      name: "Zedny",
+      img: "/assets/images/projects/zedny.png",
+      link: "https://zedny.com",
+      category: "web development",
+    },
+    {
+      name: "MacQueen",
+      img: "/assets/images/projects/macqueen.webp",
+      link: "https://macqueen.co",
+      category: "web development",
+    },
+  ],
+  Skills: [
+    {
+      name: "HTML",
+      percentage: 100,
+    },
+    {
+      name: "CSS",
+      percentage: 100,
+    },
+    {
+      name: "JavaScript",
+      percentage: 95,
+    },
+    {
+      name: "PHP",
+      percentage: 70,
+    },
+    {
+      name: "Python",
+      percentage: 50,
+    },
+    {
+      name: "Node.js",
+      percentage: 95,
+    },
+    {
+      name: "Next js",
+      percentage: 90,
+    },
+    {
+      name: "React",
+      percentage: 90,
+    },
+
+    {
+      name: "Laravel",
+      percentage: 70,
+    },
+    {
+      name: "MySQL",
+      percentage: 90,
+    },
+    {
+      name: "MongoDB",
+      percentage: 90,
+    },
+    {
+      name: "SQL",
+      percentage: 90,
+    },
+    {
+      name: "Git",
+      percentage: 90,
+    },
+    {
+      name: "Github",
+      percentage: 90,
+    },
+    {
+      name: "Docker",
+      percentage: 70,
+    },
+  ],
+  Services: [
+    {
+      name: "Web Development",
+      description:
+        " High-quality, beautiful and scalable Web Sites at the professional level. ",
+      img: "/assets/images/services/icon-dev.svg",
+    },
+    {
+      name: "Mobile Development",
+      description:
+        "Professional development of applications for Android And IOS.",
+      img: "/assets/images/services/icon-app.svg",
+    },
+    {
+      name: "Graphic Design",
+      description:
+        "High-quality design of logos, brochures, and other marketing materials.",
+      img: "/assets/images/services/graphic-design.png",
+    },
+    {
+      name: "Ui/UX Design",
+      description: "High-qualityand beautiful design of user interfaces.",
+      img: "/assets/images/services/ui-ux.png",
+    },
+  ],
+  ContactData: [
+    {
+      name: "Phone",
+      value: "+201069017890",
+      link : "tel:+201069017890",
+      icon : "phone-portrait-outline",
+    },
+    {
+      name: "Email",
+      value: "kazouya25@gmail",
+      link : "mailto:kazouya25@gmail",
+      icon : "mail-outline",
+    },
+    {
+      name: "Address",
+      value: "Mansoura Dakahlia Governorate, Egypt",
+      link : "#",
+      icon : "location-outline",
+    },
+  ],
+  SocialMedia: [
+    {
+      name: "Github",
+      link: "https://github.com/sabrydawood",
+      icon: "logo-github",
+    },
+    {
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/in/sabrydawood/",
+      icon: "logo-linkedin",
+    },
+    // {
+    //   name: "Twitter",
+    //   link: "https://twitter.com/sabry_dawood79",
+    //   icon: "logo-twitter",
+    // },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/sabry_dawood79",
+      icon: "logo-instagram",
+    },
+    {
+      name: "Discord",
+      link: "https://discord.gg/7qhtDWPCAb",
+      icon: "logo-discord",
+    },
+    {
+      name: "Whatsapp",
+      link: "https://api.whatsapp.com/send?phone=201069017890",
+      icon: "logo-whatsapp",
+    },
+    {
+      name: "Facebook",
+      link: "https://www.facebook.com/sabry.dawood.79",
+      icon: "logo-facebook",
+    },
+  ],
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,136 @@
 "use strict";
+const NavbBarButtons = ["About", "Projects", "Blog", "Contact"];
 
+window.addEventListener("DOMContentLoaded", function () {
+  const { Projects, Skills, Services, SocialMedia, ContactData } = siteData;
+  const navbarList = document.getElementById("navbarList");
+  const contactList = document.getElementById("contactList");
+  const socialList = document.getElementById("socialList");
+  const projectsList = document.getElementById("projectList");
+  const skillList = document.getElementById("skillsList");
+  const serviceList = document.getElementById("serviceList");
+  if (NavbBarButtons.length) {
+    NavbBarButtons.map((button, index) => {
+      const li = document.createElement("li");
+      li.classList.add("navbar-item");
+      li.innerHTML = `
+        <button class="navbar-link ${index === 0 ? "active" : ""}" 
+        id="nav_${button.toLowerCase()}"
+        onclick="handleToggleNavigation('${button}')"
+        >${button}</button>
+      `;
+      navbarList.appendChild(li);
+    });
+  }
+  if (ContactData.length) {
+    ContactData.map((contact) => {
+      const li = document.createElement("li");
+      li.classList.add("contact-item");
+      li.innerHTML = `
+        <div class="icon-box">
+          <ion-icon name="${contact.icon}"></ion-icon>
+        </div>
+        <div class="contact-info">
+          <p class="contact-title">${contact.name}</p>
+          <a rel="noopener" target="_blank" href="${contact.link}" class="contact-link">${contact.value}</a>
+        </div>
+      `;
+      contactList.appendChild(li);
+    });
+  }
+  if (SocialMedia.length) {
+    SocialMedia.map((social) => {
+      const li = document.createElement("li");
+      li.classList.add("social-item");
+      li.innerHTML = `
+        <a rel="noopener" href="${social.link}" target="_blank" class="social-link">
+          <ion-icon name="${social.icon}"></ion-icon>
+        </a>
+      `;
+      socialList.appendChild(li);
+    });
+  }
+  if (Projects.length) {
+    Projects.map((project) => {
+      const li = document.createElement("li");
+      li.classList.add("project-item");
+      li.classList.add("active");
+      li.setAttribute("data-filter-item", project.name);
+      li.innerHTML = `
+        <a href="${project.link}">
+          <figure class="project-img">
+            <div class="project-item-icon-box">
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+            <img src="${project.img}" alt="preview image" loading="lazy" />
+          </figure>
+          <h3 class="project-title">${project.name}</h3>
+        </a>
+      `;
+      projectsList.appendChild(li);
+    });
+  }
+  if (Skills.length) {
+    Skills.map((skill) => {
+      const li = document.createElement("li");
+      li.classList.add("skills-item");
+      li.innerHTML = `
+        <div class="title-wrapper">
+          <h5 class="h5">${skill.name}</h5>
+          <data value="${skill.percentage}"></data>
+        </div>
+        <div class="skill-progress-bg">
+          <div class="skill-progress-fill" style="width: ${skill.percentage}%"></div>
+        </div>
+      `;
+      skillList.appendChild(li);
+    });
+  }
+  if (Services.length) {
+    Services.map((service) => {
+      const li = document.createElement("li");
+      li.classList.add("service-item");
+      li.innerHTML = `
+        <div class="service-icon-box">
+          <img src="${service.img}" alt="${service.name}" width="60"/>
+        </div>
+        <div class="service-content-box">
+          <h4 class="h4 service-item-title">${service.name}</h4>
+          <p class="service-item-text">
+            ${service.description}
+          </p>
+        </div>
+      `;
+      serviceList.appendChild(li);
+    });
+  }
+});
+
+const pages = document.querySelectorAll("[data-page]");
+function handleToggleNavigation(page) {
+  NavbBarButtons.map((button, index) => {
+    const label = document.getElementById(`nav_${button.toLowerCase()}`);
+    if (button.toLowerCase() === page.toLowerCase()) {
+      label.classList.add("active");
+    } else {
+      label.classList.remove("active");
+    }
+  });
+  Object.values(pages).forEach((pageItem) => {
+    if (pageItem.dataset.page === page.toLowerCase()) {
+      pageItem.classList.add("active");
+    } else {
+      pageItem.classList.remove("active");
+    }
+  });
+  // const isHavePage = pages.find((pageItem) => pageItem.dataset.page === page);
+  // console.log({ isHavePage });
+  // if (isHavePage) {
+  //   navigationLinks[0].classList.add("active");
+  //   navigationLinks[1].classList.remove("active");
+  //   navigationLinks[2].classList.remove("active");
+  // }
+}
 // element toggle function
 const elementToggleFunc = function (elem) {
   elem.classList.toggle("active");
@@ -114,26 +245,6 @@ for (let i = 0; i < formInputs.length; i++) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
-    }
-  });
-}
-
-// page navigation variables
-const navigationLinks = document.querySelectorAll("[data-nav-link]");
-const pages = document.querySelectorAll("[data-page]");
-
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
     }
   });
 }
